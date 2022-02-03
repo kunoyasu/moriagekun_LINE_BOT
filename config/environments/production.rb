98b82b56cbbee8eb7c291076293ec93e6e8e6782
YasuyukiKuno <kunoyasuyuki@yahoo.co.jp>
2022-02-03 20:37:49 +0900

    コメントアウト

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,5 +94,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.hosts << '.ngrok.io'
+  # config.hosts << '.ngrok.io'
 end
